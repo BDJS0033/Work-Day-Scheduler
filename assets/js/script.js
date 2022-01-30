3f641dd79abf377ac74e7b93cc7ad95c3247a5fe
SAB <sevanboyadjian@gmail.com>
2022-01-29 19:32:37 -0500

    creating working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,78 @@
-//Show current Date and time with Moment.js
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+///Header current Date and time with Moment.js
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-//Starting with variables
+//initializing variable
 var workDay = []
-var  x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-var displayHour = ""
 
-//function saveText();
+//for loop creating array for time. 24Hr/12Hr clock. 9AM - 5PM; 8 hour block. 17:00 = 5:00PM
+for (time = 9; time <= 17; time++) {
+  var x = time - 9
+  var workData = ""
+
+//variable for displaying time AM/PM
+var displayTime = 0;
+var ampm = "";
+
+//if & else/if statement(s) for displaying time before/after 12PM.
+if(time === 12) {
+  displayTime = 12
+  ampm = "pm"
+} else if (time > 12) {
+  displayTime = time - 12;
+  ampm = "pm";
+} else if (time < 12) {
+  displayTime = time;
+  ampm = "am";
+}
+
+//creating string to return object(time)
+ displayTime = displayTime.toString()
+
+workData = {
+  x: x,
+  displayTime: displayTime,
+  time: time,
+  ampm: ampm,
+  workData: workData,
+}
+
+//appending new elements to array to return data 
+workDay.push(workData)
+
+}
+//End of array. 
+
+// Start of functions()
+
+//save data to localStorage
+function saveworkData() {
+  localStorage.setItem("workDay", JSON.stringify(workDay));
+}
+
+//Diplay time in time slots
+function displayworkData() {
+  workData.forEach(function (hour) {
+    $(`#${hour.id}`).val(hour.workData)
+  })    
+}
+
+//Starting function to insert data in the time slots
+function insertworkData() {
+  var insertData = JSON.parse(localStorage.getItem("workDay"));
+
+  if (insertData) {
+    workDay = insertData
+
+  }
+
+  saveworkData()
+  displayworkData()
+
+}
 
 
-//for (var i = 0; i < x.length; i++) {
-  //  var workDay = localStorage.getItem(x[i]);
 
-//$("#saveBtn").on("click", () => {
-  //  saveText(0);
-//});
-
-//creates field for time
-
-
-//var currentTime = JSON.parse(localStorage.getItem("evemts"));
-
-//save button
+// fucntion to 
+//assure save button functions appropriately
 //$(".saveBtn").on("click", function(event) {
-//  event.preventDefault();
-
-
+  // event.preventDefault();
